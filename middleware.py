f04181c84d5902e52285619eeb606535a85bec4e
fruitschen <fruitschen@gmail.com>
2010-06-18 13:28:40 +0000

    exclude specific urls from maintenance mode
    git-svn-id: http://svn2.andybak.webfactional.com/lilytest/trunk/myproject/maintenancemode@1000 66bee64b-c385-4339-a405-844911637fae

--- middleware.py
+++ middleware.py
@@ -17,18 +17,25 @@ class MaintenanceModeMiddleware(object):
         if request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS:
             pass # TODO I currently don't trust internal IPs with Webfaction's proxying although it could just be a middleware order problem
             #return None
-        
+
         # Allow acess if the user doing the request is logged in and a
         # staff member.
         if hasattr(request, 'user') and request.user.is_staff:
             return None
-        
+
         # Allow admin
         if not MAINTENANCE_MODE_ADMIN and request.META['PATH_INFO'].startswith(urlresolvers.reverse('admin:index')):
             return None
-        
+
+        # Allow specific urls
+        allow_urls = getattr(settings, 'MAINTENANCE_MODE_ALLOW_URLS', None)
+        if allow_urls:
+            for item in allow_urls:
+                if request.META['PATH_INFO'].startswith(item):
+                    return None
+
         # Otherwise show the user the 503 page
         resolver = urlresolvers.get_resolver(None)
-        
+
         callback, param_dict = resolver._resolve_special('503')
         return callback(request, **param_dict)
