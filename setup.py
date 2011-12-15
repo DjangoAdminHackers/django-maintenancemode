7470f01b83184fd16372a97dddba191be2f54021
fruitschen <fruitschen@gmail.com>
2011-12-15 15:29:14 +0800

    converted to git

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='django-maintenancemode',
+      version='0.1',
+      description='django maintenancemode',
+      author='Andy Baker',
+      author_email='andy@andybak.net',
+      packages=find_packages(),
+)
